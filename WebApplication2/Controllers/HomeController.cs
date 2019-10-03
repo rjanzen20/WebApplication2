4299a8bceb61f54018c1288a768f1c388c2bed6c
p3dr4m <p3dr4m@hotmail.com>
2019-10-03 09:55:20 -0700

    Add Link to Pedram/Robert

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -39,5 +39,9 @@ namespace WebApplication2.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult Pedram()
+        {
+            return View();
+        }
     }
 }
